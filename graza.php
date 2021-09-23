d5ff6852a8db003176cd14228a8ad33af9ee38ef
RobertasTu <tumelis.robertas@gmail.com>
2021-09-23 17:59:02 +0300

    Update graza.php
    Perdaryta grazos skaiciuokle. Veikia

--- graza.php
+++ graza.php
@@ -26,6 +26,8 @@
 if(isset($_GET["graza"]) && !empty($_GET["graza"]) ){
 $graza = $_GET['graza'];
 
+if(is_numeric($graza) && $graza > 0) {
+
 $simtines = '';
 $penkiasdesimt = '';
 $dvidesimt = '';
@@ -35,54 +37,70 @@ $du = '';
 $vienas ='';
 $likutis = '';
 
-        if(intval($graza/100) >= 1 ) {
-            $simtines = intval($graza / 100);
-            $likutis = $graza - $simtines*100;
-        } else {
-            $simtines = 0;
-        };
+        //298
+            $simtines = intval($graza / 100); //2
+            $likutis = $graza % 100; // 98
+            $penkiasdesimt = intval($likutis / 50); // 98/50=1
+            $likutis = $likutis % 50; //98%50=48
+            $dvidesimt = intval($likutis / 20); //48/20=2
+            $likutis = $likutis % 20;
+            $desimt = intval($likutis / 10);
+            $likutis = $likutis % 10;
+            $penki = intval($likutis / 5);
+            $likutis = $likutis % 5;
+            $du = intval($likutis / 2);
+            $likutis = $likutis % 2;
+            $vienas = $likutis;
 
-    if(intval($likutis/50) >= 1) {
-        $penkiasdesimt = intval($likutis / 50);
-        $likutis = $graza - $simtines*100 - $penkiasdesimt*50;
-    } else {
-        $penkiasdesimt = 0;
-    };
 
-    if(intval($likutis/20) >= 1) {
-        $dvidesimt = intval($likutis / 20);
-        $likutis = $graza - $simtines*100 - $penkiasdesimt*50 - $dvidesimt*20;
-    } else {
-        $dvidesimt = 0;
-    };
+            // echo $likutis;
+            
 
-    if(intval($likutis/10) >= 1) {
-        $desimt = intval($likutis / 10);
-        $likutis = $graza - $simtines*100 - $penkiasdesimt*50 - $dvidesimt*20 - $desimt*10;
-    } else {
-        $desimt = 0;
-    };
+    //     } else {
+    //         $simtines = 0;
+    //     };
 
-    if(intval($likutis/5) >= 1) {
-        $penki = intval($likutis / 5);
-        $likutis = $graza - $simtines*100 - $penkiasdesimt*50 - $dvidesimt*20 - $desimt*10 - $penki*5;
-    } else {
-        $penki = 0;
-    };
+    // else if($graza < 100) (intval($likutis/50 || $graza/50) >= 1) {
+    //     $penkiasdesimt = intval($likutis / 50);
+    //     $likutis = $graza - $simtines*100 - $penkiasdesimt*50;
+    // } else {
+    //     $penkiasdesimt = 0;
+    // };
 
-    if(intval($likutis/2) >= 1) {
-        $du = intval($likutis / 2);
-        $likutis = $graza - $simtines*100 - $penkiasdesimt*50 - $dvidesimt*20 - $desimt*10 - $penki*5 - $du*2;
-    } else {
-        $du = 0;
-    };
+    // if(intval($likutis/20) >= 1) {
+    //     $dvidesimt = intval($likutis / 20);
+    //     $likutis = $graza - $simtines*100 - $penkiasdesimt*50 - $dvidesimt*20;
+    // } else {
+    //     $dvidesimt = 0;
+    // };
 
-    if(intval($likutis/1) >= 1) {
-        $vienas = intval($likutis / 1);
-        $likutis = $graza - $simtines*100 - $penkiasdesimt*50 - $dvidesimt*20 - $desimt*10 - $penki*5 - $du*2 - $vienas;
-    } else {
-        $vienas = 0;
-    };
+    // if(intval($likutis/10) >= 1) {
+    //     $desimt = intval($likutis / 10);
+    //     $likutis = $graza - $simtines*100 - $penkiasdesimt*50 - $dvidesimt*20 - $desimt*10;
+    // } else {
+    //     $desimt = 0;
+    // };
+
+    // if(intval($likutis/5) >= 1) {
+    //     $penki = intval($likutis / 5);
+    //     $likutis = $graza - $simtines*100 - $penkiasdesimt*50 - $dvidesimt*20 - $desimt*10 - $penki*5;
+    // } else {
+    //     $penki = 0;
+    // };
+
+    // if(intval($likutis/2) >= 1) {
+    //     $du = intval($likutis / 2);
+    //     $likutis = $graza - $simtines*100 - $penkiasdesimt*50 - $dvidesimt*20 - $desimt*10 - $penki*5 - $du*2;
+    // } else {
+    //     $du = 0;
+    // };
+
+    // if(intval($likutis/1) >= 1) {
+    //     $vienas = intval($likutis / 1);
+    //     $likutis = $graza - $simtines*100 - $penkiasdesimt*50 - $dvidesimt*20 - $desimt*10 - $penki*5 - $du*2 - $vienas;
+    // } else {
+    //     $vienas = 0;
+    // };
 
 
 
@@ -95,8 +113,15 @@ $likutis = '';
     echo 'Monetos po 2 Eur = '.$du.'vnt'.'<br>';
     echo 'Monetos po 1 Eur = '.$vienas.'vnt'.'<br>';
 
+    echo 'Pasitikrinimui'.'<br>';
+    echo '100*'.$simtines.'+'.'50*'.$penkiasdesimt.'+'.'20*'.$dvidesimt.'+'.'10*'.$desimt.'+'.'5*'.$penki.'+'.'2*'.$du.'+'.'1*'.$vienas;
+
+} else { 
+    echo 'Iveskite teigiamą skaitinę vertę';
+}
 }
 
+45
 
 
 ?>
